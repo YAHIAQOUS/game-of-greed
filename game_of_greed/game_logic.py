0a33b1360b29beedff86c1b77bd51287ac3a6b54
Omardyab <omarzadyab@gmail.com>
2021-08-01 17:24:50 +0300

    roll_dice done and tested

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,13 +1,20 @@
 import abc
-
+from random import randint
 
 # from abc import ABC 
 class GameLogic:
 
     @staticmethod
     def calculate_score(x:tuple)-> int:
+
         pass
 
     @staticmethod 
-    def roll_dice(num:int)->tuple:
-        pass
+    def roll_dice(num=6)->tuple:
+        list_dice=[]
+        for i in range(num):
+            list_dice.append(randint(1,6))
+        return tuple(list_dice)
+        
+        
+
